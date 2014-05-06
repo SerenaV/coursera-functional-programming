4e59930968cfa39ef2a391c302878fd2b91b0027
Serena Vandersteen <SerenaV@iqmetrix.com>
2014-05-06 14:14:25 -0500

    Assignment 1; pascal working, rest un-tested

---
+++ A1/recfun/.worksheet/src/recfun.Assignment1Worksheet.scala
@@ -0,0 +1,11 @@
+package recfun
+import Main._
+
+object Assignment1Worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(103); 
+  println("Welcome to the Scala worksheet");$skip(17); val res$0 = 
+  
+  pascal(4,4);System.out.println("""res0: Int = """ + $show(res$0))}
+  
+  
+  
+}
---
+++ A1/recfun/target/scala-2.10/classes/recfun/Assignment1Worksheet.sc
@@ -0,0 +1,11 @@
+package recfun
+import Main._
+
+object Assignment1Worksheet {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  pascal(4,4)                                     //> res0: Int = 1
+  
+  
+  
+}
