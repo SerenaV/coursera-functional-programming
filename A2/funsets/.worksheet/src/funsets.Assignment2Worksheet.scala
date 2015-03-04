0ff71b342b6068b871da95596adffb0fcdc9547d
SerenaV <SerenaV@iqmetrix.com>
2015-03-04 15:29:39 -0600

    All the deletions to clean up...

--- A2/funsets/.worksheet/src/funsets.Assignment2Worksheet.scala
+++
@@ -1,5 +0,0 @@
-package funsets
-
-object Assignment2Worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(90); 
-  println("Welcome to the Scala worksheet")}
-}
--- A2/funsets/target/scala-2.10/classes/funsets/Assignment2Worksheet.sc
+++
@@ -1,5 +0,0 @@
-package funsets
-
-object Assignment2Worksheet {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-}
